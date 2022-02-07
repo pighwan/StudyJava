ec5ddd6656abc34870c5a9f59cd33546bb2dc1e0
Administrator <Administrator@User-2021WVGBNH>
2022-02-07 16:33:40 +0900

    11.2 입력과 출력 커밋

--- src/j03_Programming/OkJavaGoInHomeInput.java
+++ src/j03_Programming/OkJavaGoInHomeInput.java
@@ -6,11 +6,14 @@ import j03_Programming.Elevator;
 import j03_Programming.Security;
 import j03_Programming.Lighting;
 
-public class OkJavaGoInHomeInput {
 
+public class OkJavaGoInHomeInput {
+	
+	// parameter, 매개변수
 	public static void main(String[] args) {
-		String id = JOptionPane.showInputDialog("Enter a ID");
-		String bright = JOptionPane.showInputDialog("Enter a Bright Level");
+		
+		String id = args[0];
+		String bright = args[1];
 		
 		// Elevator call
 		Elevator myElevator = new Elevator(id);
