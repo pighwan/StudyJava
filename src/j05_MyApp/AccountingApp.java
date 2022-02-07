6bc3f630217a9f408a6e5a6a404922827733a0ba
Administrator <Administrator@User-2021WVGBNH>
2022-02-07 17:56:06 +0900

    alt shift l이 단축키

--- src/j05_MyApp/AccountingApp.java
+++ src/j05_MyApp/AccountingApp.java
@@ -3,14 +3,26 @@ package j05_MyApp;
 public class AccountingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Value of supply : "+12345.0); // 공급가
-		System.out.println("VAT : "+(12345.0*0.1)); // 부가가치세
-		System.out.println("Total : "+(12345.0 + 12345.0*0.1));
-		System.out.println("Expense : "+(12345.0*0.3)); 
-		System.out.println("Income : "+(12345.0 - 12345.0*0.3)); // 전체 수익
-		System.out.println("Dividend 1 : "+(12345.0 - 12345.0*0.3) * 0.5);
-		System.out.println("Dividend 2 : "+(12345.0 - 12345.0*0.3) * 0.3);
-		System.out.println("Dividend 3 : "+(12345.0 - 12345.0*0.3) * 0.2);
+				
+		double valueOfSupply = 12345.0;
+		double vatRate = 0.1; // 부가 가치세
+		double expenseRate = 0.3;
+		double vat = valueOfSupply*vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply - expense;
+		double dividend1 = income * 0.5;
+		double dividend2 = income * 0.3;
+		double dividend3 = income * 0.2;
+		
+		System.out.println("Value of supply : "+valueOfSupply); // 공급가			
+		System.out.println("VAT : "+vat); // 부가가치세		
+		System.out.println("Total : "+total);			
+		System.out.println("Expense : "+expense); 		
+		System.out.println("Income : "+income); // 전체 수익		
+		System.out.println("Dividend 1 : "+dividend1);		
+		System.out.println("Dividend 2 : "+dividend2);		
+		System.out.println("Dividend 3 : "+dividend3);
 		
 		
 	} // main
