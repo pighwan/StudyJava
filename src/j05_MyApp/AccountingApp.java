d154c356d49c7c746c9fdbf6949a9e11fb0b6e7c
Administrator <Administrator@User-2021WVGBNH>
2022-02-07 17:44:45 +0900

    find/replace

--- src/j05_MyApp/AccountingApp.java
+++ src/j05_MyApp/AccountingApp.java
@@ -3,9 +3,15 @@ package j05_MyApp;
 public class AccountingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Value of supply : "+10000.0);
-		System.out.println("VAT : "+(10000.0*0.1));
-		System.out.println("VAT : "+(10000.0*0.1));
+		System.out.println("Value of supply : "+12345.0); // 공급가
+		System.out.println("VAT : "+(12345.0*0.1)); // 부가가치세
+		System.out.println("Total : "+(12345.0 + 12345.0*0.1));
+		System.out.println("Expense : "+(12345.0*0.3)); 
+		System.out.println("Income : "+(12345.0 - 12345.0*0.3)); // 전체 수익
+		System.out.println("Dividend 1 : "+(12345.0 - 12345.0*0.3) * 0.5);
+		System.out.println("Dividend 2 : "+(12345.0 - 12345.0*0.3) * 0.3);
+		System.out.println("Dividend 3 : "+(12345.0 - 12345.0*0.3) * 0.2);
+		
 		
 	} // main
 
