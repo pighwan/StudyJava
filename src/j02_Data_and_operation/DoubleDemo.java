4ad48abd008e4e5d42a6a8fe158a40856d2cc43c
Notebook <Administrator@121.136.98.159>
2022-02-12 00:54:11 +0900

    변수의 선언과 할당, 정수와 실수, 문자열

---
+++ src/j02_Data_and_operation/DoubleDemo.java
@@ -0,0 +1,14 @@
+package j02_Data_and_operation;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a;
+		// int a;  // Type mismatch: cannot convert from double to int
+		a = 1.1;
+		
+		System.out.println(a+1.1); // 2.2
+
+	} // main
+
+} // class
