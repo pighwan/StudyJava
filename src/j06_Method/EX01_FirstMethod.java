9d1b0ca5ede64d9278fb5de0698ee8ff5f5b4366
Notebook <Administrator@121.168.84.48>
2022-02-13 23:31:15 +0900

    메소드에 입력값을 받는 방법 : 매개변수(parameter)와 인자(argument)를 이용!

--- src/j06_Method/EX01_FirstMethod.java
+++ src/j06_Method/EX01_FirstMethod.java
@@ -4,6 +4,7 @@ public class EX01_FirstMethod {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Method");
+		// println : "" 안의 내용을 출력해주는 메소드를 사용
 		System.out.println(Math.floor(1.1)); // floor : 내림
 		// Math 안에 소속되어 있는 floor라는 메소드를 사용
 		
