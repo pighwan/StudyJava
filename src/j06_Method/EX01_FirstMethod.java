c10a4005b2aac43179ea1787363d3efe50ea3b75
Notebook <Administrator@121.168.84.48>
2022-02-13 23:18:48 +0900

    Method(함수): 복잡한 것을 정리해서 다시 단순하게 만드는 정리정돈의 도구
    Refactor-Extract Method 통해서 메소드화

---
+++ src/j06_Method/EX01_FirstMethod.java
@@ -0,0 +1,13 @@
+package j06_Method;
+
+public class EX01_FirstMethod {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Method");
+		System.out.println(Math.floor(1.1)); // floor : 내림
+		// Math 안에 소속되어 있는 floor라는 메소드를 사용
+		
+
+	} // main
+
+} // class
---
+++ src/j06_Method/Ex02_WhyMethod.java
@@ -0,0 +1,22 @@
+package j06_Method;
+
+public class Ex02_WhyMethod {
+
+	public static void main(String[] args) {
+		
+		// 100000000
+		printTwoTimesA();
+		// 100000000
+		printTwoTimesA();
+		// 100000000
+		printTwoTimesA();
+		
+	} // main
+
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("a");
+		System.out.println("a");
+	} // printTwoTimesA
+
+} // class
