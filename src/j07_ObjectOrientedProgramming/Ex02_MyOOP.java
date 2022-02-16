6446f75ab5a0227dfcdba1d8fb1bae1816e3efa1
Notebook <Administrator@121.168.84.48>
2022-02-16 22:25:03 +0900

    Print라는 클래스를 만들고 그 안에 delimiter, A, B라는 메소드를 넣음

--- src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
+++ src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
@@ -1,33 +1,38 @@
 package j07_ObjectOrientedProgramming;
 
+class Print{
+	// Print라는 클래스를 만들고 그 안에 delimiter, A, B라는 메소드를 넣음
+	public static String delimiter = "";
+	
+	public static void A() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	} // A
+	
+	public static void B() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	} // B
+} // Print
+
 public class Ex02_MyOOP {	
 	public static void main(String[] args) {
-		delimiter = "----";
-		printA();
-		printA();		
-		printB();
-		printB();
+		Print.delimiter = "----";
+		Print.A();		
+		Print.A();
+		Print.B();
+		Print.B();
 		
-		delimiter = "****";
-		printA();
-		printA();		
-		printB();
-		printB();		
+		Print.delimiter = "****";
+		Print.A();
+		Print.A();		
+		Print.B();
+		Print.B();		
 
 	} // main
 	
-	public static String delimiter = "";
 	
-	public static void printA() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	} // printA
-	
-	public static void printB() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	} // printB
 
 } // class
