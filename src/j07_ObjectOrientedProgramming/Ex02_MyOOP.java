b0f548e381459c10e35fa60484eba0d77b24ac84
Notebook <Administrator@121.168.84.48>
2022-02-16 22:34:35 +0900

    MyOOP 안에 있던 Print 클래스를 따로 새 클래스로 만들어서 거기 있는 메소드를 가져와봄

--- src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
+++ src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
@@ -1,38 +1,19 @@
 package j07_ObjectOrientedProgramming;
 
-class Print{
-	// Print라는 클래스를 만들고 그 안에 delimiter, A, B라는 메소드를 넣음
-	public static String delimiter = "";
-	
-	public static void A() {
-		System.out.println(delimiter);
-		System.out.println("A");
-		System.out.println("A");
-	} // A
-	
-	public static void B() {
-		System.out.println(delimiter);
-		System.out.println("B");
-		System.out.println("B");
-	} // B
-} // Print
-
 public class Ex02_MyOOP {	
 	public static void main(String[] args) {
-		Print.delimiter = "----";
-		Print.A();		
-		Print.A();
-		Print.B();
-		Print.B();
+		Ex03_Print.delimiter = "----";
+		Ex03_Print.A();		
+		Ex03_Print.A();
+		Ex03_Print.B();
+		Ex03_Print.B();
 		
-		Print.delimiter = "****";
-		Print.A();
-		Print.A();		
-		Print.B();
-		Print.B();		
+		Ex03_Print.delimiter = "****";
+		Ex03_Print.A();
+		Ex03_Print.A();		
+		Ex03_Print.B();
+		Ex03_Print.B();		
 
-	} // main
-	
-	
+	} // main	
 
 } // class
