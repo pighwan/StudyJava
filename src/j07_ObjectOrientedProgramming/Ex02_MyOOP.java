3809c729d0e488818fbb1aa239892b90b6418ef8
Notebook <Administrator@121.168.84.48>
2022-02-16 22:52:40 +0900

    기존의 Print 클래스에서 static을 빼주고 p1, p2라는 인스턴스를 만들어서 더 간편화 (기존꺼는 ex02, ex03로 새로 한 것은 ex04, ex05로)

--- src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
+++ src/j07_ObjectOrientedProgramming/Ex02_MyOOP.java
@@ -2,18 +2,30 @@ package j07_ObjectOrientedProgramming;
 
 public class Ex02_MyOOP {	
 	public static void main(String[] args) {
-		Ex03_Print.delimiter = "----";
-		Ex03_Print.A();		
-		Ex03_Print.A();
-		Ex03_Print.B();
-		Ex03_Print.B();
+		Ex03_Print1.delimiter = "----";
+		Ex03_Print1.A();		
+		Ex03_Print1.A();
+		Ex03_Print1.B();
+		Ex03_Print1.B();
 		
-		Ex03_Print.delimiter = "****";
-		Ex03_Print.A();
-		Ex03_Print.A();		
-		Ex03_Print.B();
-		Ex03_Print.B();		
-
+		Ex03_Print1.delimiter = "****";
+		Ex03_Print1.A();
+		Ex03_Print1.A();		
+		Ex03_Print1.B();
+		Ex03_Print1.B();	
+		
+		System.out.println("\n");
+		Ex03_Print1.delimiter = "----";
+		Ex03_Print1.A();	
+		Ex03_Print1.delimiter = "****";
+		Ex03_Print1.A();
+		Ex03_Print1.delimiter = "----";
+		Ex03_Print1.A();	
+		Ex03_Print1.delimiter = "****";
+		Ex03_Print1.A();
+		
+		
+		
 	} // main	
 
 } // class
--- src/j07_ObjectOrientedProgramming/Ex04_Print2.java
+++ src/j07_ObjectOrientedProgramming/Ex04_Print2.java
@@ -1,16 +1,16 @@
 package j07_ObjectOrientedProgramming;
 
-class Ex03_Print1 {
+class Ex04_Print2 {
 	// Print라는 클래스를 만들고 그 안에 delimiter, A, B라는 메소드를 넣음
-	public static String delimiter = "";
+	public String delimiter = "";
 	
-	public static void A() {
+	public void A() {
 		System.out.println(delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	} // A
 	
-	public static void B() {
+	public void B() {
 		System.out.println(delimiter);
 		System.out.println("B");
 		System.out.println("B");
