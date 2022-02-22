de577415138987592b50cd58e4ce1f85e1ae924f
Notebook <Administrator@222.99.115.23>
2022-02-22 21:06:04 +0900

    1) 생성자 : 초기에 주입할 필요가 있는 값을 전달하거나 초기에 작업을 수행하도록 할때 쓴다
    2) this : 클래스가 인스턴스화되었을 때 인스턴스를 가르키는 특수한 이름

--- src/j07_ObjectOrientedProgramming/Ex04_Print2.java
+++ src/j07_ObjectOrientedProgramming/Ex04_Print2.java
@@ -4,14 +4,18 @@ class Ex04_Print2 {
 	// Print라는 클래스를 만들고 그 안에 delimiter, A, B라는 메소드를 넣음
 	public String delimiter = "";
 	
+	public Ex04_Print2(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	
 	public void A() {
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	} // A
 	
 	public void B() {
-		System.out.println(delimiter);
+		System.out.println(this.delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	} // B
