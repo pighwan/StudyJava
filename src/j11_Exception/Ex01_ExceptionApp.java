2a0bf49f2297909069a890b3c710bee3a70679ae
Notebook <Administrator@222.99.115.23>
2022-03-05 20:03:40 +0900

    Exception
    - e.getMessage : 에러의 원인이 무엇인지 알려줌
    
    - e.printStackTrace(); : 어떤 에러가 뜨는지 표시해줌(빨간색 에러메세지)

--- src/j11_Exception/Ex01_ExceptionApp.java
+++ src/j11_Exception/Ex01_ExceptionApp.java
@@ -25,15 +25,22 @@ public class Ex01_ExceptionApp {
 		System.out.println(1);
 		try {
 			System.out.println(2);
-			System.out.println(scores[3]); // ArrayIndexOutOfBoundsException이 실행되고 그 밑에 명령은 실행될 기회를 받지 못함
+			//System.out.println(scores[3]); // ArrayIndexOutOfBoundsException이 실행되고 그 밑에 명령은 실행될 기회를 받지 못함
 			System.out.println(3);
 			System.out.println(2 / 0);
 			System.out.println(4);
 		} catch (ArithmeticException e) {
-			System.out.println("잘못된 계산이넹~");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("없는 값이여요!!");
+			System.out.println("계산이 잘못된거 같아용!"+e.getMessage()); // e.getMessage(): 에러의 원인이 무엇인지 알려줌
+			e.printStackTrace(); // 어떤 에러가 뜨는지 표시해줌
+		} catch (Exception e) {
+			System.out.println("뭔가 이상합니다. 오류가 발생했습니다."+e.getMessage()); 
+			e.printStackTrace(); 
 		}
+//		} catch (ArithmeticException e) {
+//			System.out.println("잘못된 계산이넹~");
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("없는 값이여요!!");
+//		}
 		System.out.println(5);
 		
 	} // main
