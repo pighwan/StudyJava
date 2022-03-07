86d4b73e84e6a0897c3078fa6df5bb9ce8194548
Notebook <Administrator@121.137.222.137>
2022-03-07 23:15:26 +0900

    RuntimeException
    - 이것을 통해 직접 예외를 발생시킬 수 있다.
    
    - try catch문을 이용하거나 throws를 추가해주면 try catch문을 안써줘도 됨

---
+++ src/j11_Exception/Ex05_ThrowException.java
@@ -0,0 +1,18 @@
+package j11_Exception;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Ex05_ThrowException {
+
+	public static void main(String[] args) throws IOException {
+//		try {
+//			FileWriter f = new FileWriter("./data.txt");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} 주석처리 한 것처럼 하거나 아예 위쪽에 throws IOException를 명시해줌
+		FileWriter f = new FileWriter("./data.txt");
+
+	} // main
+
+} // class
