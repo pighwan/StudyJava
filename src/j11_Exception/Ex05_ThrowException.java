eef3355554717586a38704178e692aa80d96d7ff
Notebook <Administrator@121.137.222.137>
2022-03-08 23:48:22 +0900

    내일부터는 웹 들어가자~~!

--- src/j11_Exception/Ex05_ThrowException.java
+++ src/j11_Exception/Ex05_ThrowException.java
@@ -12,6 +12,8 @@ public class Ex05_ThrowException {
 //			e.printStackTrace();
 //		} 주석처리 한 것처럼 하거나 아예 위쪽에 throws IOException를 명시해줌
 		FileWriter f = new FileWriter("./data.txt");
+		
+		// 내일부터는 웹
 
 	} // main
 
